513808219247587488677b647e42ad5f7598985c
Captain Throwback <captainthrowback@hotmail.com>
2020-11-17 19:48:38 -0500

    ime: updates to use twrp-common for decryption
    - Update recovery.fstab and remove twrp.fstab
    - Remove unnecessary files
    
    Change-Id: I61a7e138c696f3afee3217247df68bedc869cc6d

--- screenshot_patch/Android.mk
+++ screenshot_patch/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := $(TARGET_DEVICE)_screenshot
 LOCAL_MODULE_TAGS := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/postinstall
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)/system/bin
 LOCAL_REQUIRED_MODULES := teamwin
 LOCAL_POST_INSTALL_CMD += \
     $(hide) sed -i 's:power+voldown:power+home:' $(TARGET_RECOVERY_ROOT_OUT)/twres/portrait.xml; \
